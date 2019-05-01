2ed7928f168ce745f439335e4f042994cff355b5
ihakapink <ihakapink@gmail.com>
2019-05-02 11:30:45 +1200

    updated db functions for getUser

--- server/db/db.js
+++ server/db/db.js
@@ -2,11 +2,15 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
-module exports = {
-getUsers,
-getUser
+module.exports = {
+  getUsers,
+  getUser
 }
 
 function getUsers (db = connection) {
   return db('users').select
+}
+
+function getUser (db = connection) {
+  return db('users').where('id', id).first()
 }
