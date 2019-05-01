5689eb94b07799a998d4a01f06c51e238ab5375b
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-02 11:13:43 +1200

    Created Modules migration

--- migrations/20190502105846_modules.js
+++ migrations/20190502105846_modules.js
@@ -1,9 +1,10 @@
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('', (table) => {
-
+  return knex.schema.createTable('modules', (table) => {
+    table.integer('id').primary()
+    table.string('title')
   })
 }
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('')
+  return knex.schema.dropTable('modules')
 }
