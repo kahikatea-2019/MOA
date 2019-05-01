bfe26f262b21d72fe5d6a4e051ba9bb237141d84
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-02 11:07:29 +1200

    Created Students migration

--- migrations/20190502105812_students.js
+++ migrations/20190502105812_students.js
@@ -1,9 +1,13 @@
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('', (table) => {
-
+  return knex.schema.createTable('students', (table) => {
+    table.integer('id').primary()
+    table.integer('user_id')
+    table.integer('cohort_id')
+    table.string('first_name')
+    table.string('last_name')
   })
 }
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('')
+  return knex.schema.dropTable('students')
 }
