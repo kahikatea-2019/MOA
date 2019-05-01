a7fcfb74883f7e348860e18f7f764bf83049323d
MurdocAV <azaan.virk1@gmail.com>
2019-05-02 11:26:02 +1200

    3/8 seeds completed

---
+++ seeds/cohorts.js
@@ -0,0 +1,10 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('cohorts').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cohorts').insert([
+        {id: 1, name: 'Kahikatea', year: 2019, campus: 'Auckland'}
+      ])
+    })
+}
