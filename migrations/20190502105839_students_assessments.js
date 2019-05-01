b7edfad87744720afeda8bc69fda4c6a2dea2e41
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-02 11:12:23 +1200

    Created Students_Assessments migration

--- migrations/20190502105839_students_assessments.js
+++ migrations/20190502105839_students_assessments.js
@@ -1,9 +1,14 @@
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('', (table) => {
-
+  return knex.schema.createTable('students_assessments', (table) => {
+    table.integer('id').primary()
+    table.integer('student_id')
+    table.integer('assessment_id')
+    table.integer('status_id')
+    table.string('evidence')
+    table.string('date_modified')
   })
 }
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('')
+  return knex.schema.dropTable('students_assessments')
 }
