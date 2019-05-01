08cec924ac295db81ebbd2ed6c45cff782c8cf47
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-02 11:16:29 +1200

    Created Exercises migration

--- migrations/20190502105851_exercises.js
+++ migrations/20190502105851_exercises.js
@@ -1,9 +1,11 @@
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('', (table) => {
-
+  return knex.schema.createTable('exercises', (table) => {
+    table.integer('id').primary()
+    table.integer('assessment_id')
+    table.string('title')
   })
 }
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('')
+  return knex.schema.dropTable('exercises')
 }
