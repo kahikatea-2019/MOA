18748fb2bcb9d4eca51214af18783d1cd1e0bbcd
Azaan Virk <azaan.virk1@gmail.com>
2019-05-02 19:00:41 +1200

    Update cohorts.js

--- server/db/seeds/cohorts.js
+++ server/db/seeds/cohorts.js
@@ -4,7 +4,7 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('cohorts').insert([
-        {id: 1, name: 'Kahikatea', year: 2019, campus: 'Auckland'}
+        {id: 1, name: 'Kahikatea', year: '2019', campus: 'Auckland'}
       ])
     })
 }
