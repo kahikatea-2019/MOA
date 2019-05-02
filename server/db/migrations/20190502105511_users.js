f1ce95bd80ccb20088e75d6477a103e7d132327d
Anandani Mala Prakash <anandaniprakash@icloud.com>
2019-05-02 15:51:00 +1200

    updated table in users

--- server/db/migrations/20190502105511_users.js
+++ server/db/migrations/20190502105511_users.js
@@ -1,8 +1,8 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.integer('id').primary()
-    table.string('firstname')
-    table.string('lastname')
+    table.string('first_name')
+    table.string('last_name')
     table.string('email')
     table.string('hash')
     table.string('role')
