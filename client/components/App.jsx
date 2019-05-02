9d08c82172833b4f51ba6480270709418fa1d84c
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-02 16:18:16 +1200

    Fixed the Header componenet to recieve the data

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import Header from './Header'
 
 const App = () => {
   return (
     <Router>
+      <Header/>
       <Switch>
-        <h1>MOA development has begun!</h1>
+        <h1>MOA development has begun!</h1>      
       </Switch>
     </Router>
   )
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
 
-class Header extends React.Component{
-render(){
-    return(
-        <div className = 'Header'>
-        <h1><p>Name</p></h1>
-        <h2><p>Surname</p></h2>
-        <h3><p>Harakeke2019</p></h3>       
-        </div>
-    )
+export default class AddWidget extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: 'Eteroa',
+            surname: ' Lafaele',
+            cohort: 'SWOOOOOP 2019'
+        }
+    }
+
+    render () {
+        const { name ,surname , cohort} = this.state
+        return(
+            <React.Fragment>
+            <p><h1>{name}</h1></p>
+            <p><h2>{surname}</h2></p>
+            <p><h2>{cohort}</h2></p>  
+            </React.Fragment>
+            ) 
+        }
+    
+
 }
-}
+
+
+// class Header extends React.Component{
+// render(){
+//     return(
+//         <div className = 'Header'>
+//         <h1><p>Name</p></h1>
+//         <h2><p>Surname</p></h2>
+//         <h3><p>Harakeke2019</p></h3>       
+//         </div>
+//     )
+// }
+// }
 
 
 
 
 
 
-export default Header 
+// export default Header 
