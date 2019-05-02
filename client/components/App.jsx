53a6016086b15aed6b6c8294fa43710b3b49456f
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-02 12:06:21 +1200

    Added Component and added HTML tags and linked to App Component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
 const App = () => {
   return (
+    <React.Fragment>
+    <Header/>
     <h1>MOA development has begun!</h1>
+    </React.Fragment>
   )
 }
 
