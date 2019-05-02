07598a4f394b4bb296ffb68d678d6edcddd4ee3b
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-02 16:26:29 +1200

    Removed linting error

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -23,23 +23,3 @@ export default class AddWidget extends React.Component{
     
 
 }
-
-
-// class Header extends React.Component{
-// render(){
-//     return(
-//         <div className = 'Header'>
-//         <h1><p>Name</p></h1>
-//         <h2><p>Surname</p></h2>
-//         <h3><p>Harakeke2019</p></h3>       
-//         </div>
-//     )
-// }
-// }
-
-
-
-
-
-
-// export default Header 
