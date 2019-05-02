580f9ba3189c993193128f302c86b8872a472c37
Eve Te Uru Henare <evehenare1@gmail.com>
2019-05-02 15:36:19 +1200

    Created state with static assessment data, rendered list of module names

--- client/components/DropDownModule.jsx
+++ client/components/DropDownModule.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 
-function DropDownModule () {
-  const x = 'Hi Eve'
-  console.log(x)
-  return (
-    <p>yo</p>
-  )
+export default class DropDownModule extends React.Component {
+  state = {
+    module: [
+      {
+        'moduleId': 1,
+        'moduleTitle': 'Module 1: Core Programming',
+        'assessmentsId': 1,
+        'assessmentsTitle': '1. Use Git and terminal commands to manage a code base',
+        'description': 'desc',
+        'link': 'link',
+        'week_day': 'weekday',
+        'exerciseId': 1,
+        'exerciseTitle': 'https://github.com/dev-academy-challenges/taxonomic-routing'
+      },
+      {
+        'moduleId': 1,
+        'moduleTitle': 'Module 1: Core Programming',
+        'assessmentsId': 2,
+        'assessmentsTitle': '2. Use npm to manage library dependencies',
+        'description': 'desc',
+        'link': 'link',
+        'week_day': 'weekday',
+        'exerciseId': 2,
+        'exerciseTitle': 'https://github.com/dev-academy-challenges/knex-relationships-stories'
+      }
+    ]
+  }
+
+  moduleName = this.state.module.map(item => {
+    return <li>{item.moduleTitle}</li>
+  })
+
+  render () {
+    return (
+      <div>
+        <ul>
+          <li>{this.moduleName}</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default DropDownModule
