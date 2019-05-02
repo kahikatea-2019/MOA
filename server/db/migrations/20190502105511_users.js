27d920118a19c8f512e0f4a30719b0037bbf75fd
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-02 15:06:11 +1200

    updated Users migration to meet new data model

--- server/db/migrations/20190502105511_users.js
+++ server/db/migrations/20190502105511_users.js
@@ -1,8 +1,9 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.integer('id').primary()
-    table.string('name')
-    table.string('surname')
+    table.string('firstname')
+    table.string('lastname')
+    table.string('email')
     table.string('hash')
     table.string('role')
   })
