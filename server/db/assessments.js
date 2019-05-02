fd688ee101c80ddea822005099a7ef065fce5fed
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-03 11:01:39 +1200

    created getAssessments db function

---
+++ server/db/assessments.js
@@ -0,0 +1,13 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../db/knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  getAssessments
+}
+
+function getAssessments (db = connection) {
+  return db('assessments')
+    .join('modules', 'modules.id', 'assessments.module_id')
+    .select('assessment.id', 'assessment.module_id', 'modules.id', 'assessment.title', 'assessment.description', 'assessment.link', 'assessment.week_day')
+}
