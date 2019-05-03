b09c11b3be7c1fe9e319f5509aba966da71e394b
Alice Gatland <unrelated@gmail.com>
2019-05-03 13:17:20 +1200

    add userId to success return

--- server/auth/token.js
+++ server/auth/token.js
@@ -8,7 +8,8 @@ function issue (req, res) {
   res.json({
     ok: true,
     message: 'Authentication successful.',
-    token: createToken(res.locals.userId)
+    token: createToken(res.locals.userId),
+    userId: res.locals.userId
   })
 }
 
