01ac37cd861855ebb065f95243867dbc856acd2c
MurdocAV <azaan.virk1@gmail.com>
2019-05-03 15:04:52 +1200

    Fixed table names and added another user for test use

--- server/db/seeds/statuses.js
+++ server/db/seeds/statuses.js
@@ -1,9 +1,9 @@
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('statuses').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('statuses').insert([
         {id: 1, name: 'not started'},
         {id: 2, name: 'in progress'},
         {id: 3, name: 'done - awaiting review'},
--- server/db/seeds/students.js
+++ server/db/seeds/students.js
@@ -4,7 +4,8 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('students').insert([
-        { id: 1, user_id: 2, cohort_id: 1 }
+        {id: 1, user_id: 2, cohort_id: 1},
+        {id: 1, user_id: 1, cohort_id: 1}
       ])
     })
 }
