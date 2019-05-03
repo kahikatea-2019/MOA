ffca81db2f60d744a60eee2a5945c262aacd3ebc
MurdocAV <azaan.virk1@gmail.com>
2019-05-03 14:43:58 +1200

    Consistent with new wiki data-model, and pulled changes from dev branch

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import Header from './Header'
+
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <h1>MOA development has begun!</h1>
+      <Route path ='/' component={Header} />
+        <h1>MOA development has begun!</h1>      
       </Switch>
     </Router>
   )
