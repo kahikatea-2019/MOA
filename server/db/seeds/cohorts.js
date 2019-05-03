916c60a71ec237671393f6e3832ba70b4a531a5a
MurdocAV <azaan.virk1@gmail.com>
2019-05-03 14:56:42 +1200

    added more cohorts for better test coverage

--- server/db/seeds/cohorts.js
+++ server/db/seeds/cohorts.js
@@ -4,7 +4,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('cohorts').insert([
-        { id: 1, name: 'Kahikatea', year: '2019', campus: 'Auckland' }
+        {id: 1, name: 'Kahikatea', year: '2019', campus: 'Auckland'},
+        {id: 2, name: 'Kahu', year: '2019', campus: 'Auckland'},
+        {id: 3, name: 'Karearea', year: '2019', campus: 'Auckland'}
       ])
     })
 }
