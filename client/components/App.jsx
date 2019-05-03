13a71120643f16a1140dd20c95bfb51d7ce70a5d
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-03 12:24:31 +1200

    Added Dashboard component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
-
+import Dashboard from './Dashboard'
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Dashboard/>
         <h1>MOA development has begun!</h1>
       </Switch>
     </Router>
