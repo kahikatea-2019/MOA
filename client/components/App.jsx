956569b707003430570274d18bae8d5500114f9a
emily <emilycoco@me.com>
2019-05-06 11:32:13 +1200

    code style fixes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-import Header from './Header'
-import Register from './Register'
 import Login from './Login'
-
-
+import Register from './Register'
 
 const App = () => {
   return (
     <Router>
-      <Switch> 
-        <Route path ='/' component={Header} />
-        <Route path='/register' component={Register} />
+      <Switch>
         <Route path='/' component ={Login} />
+        <Route path='/register' component={Register} />
       </Switch>
     </Router>
   )
