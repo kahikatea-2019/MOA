1036ce49517959fd0df7cacee0111ec85b8b135b
Paul Renwick <prenwick@gmail.com>
2019-05-06 09:48:06 +1200

    Adjusted router test

--- tests/server/routes/submitAssessmentRoute.test.js
+++ tests/server/routes/submitAssessmentRoute.test.js
@@ -1,5 +1,4 @@
 const request = require('supertest')
-const cheerio = require('cheerio')
 
 const server = require('../../../server/server.js')
 
@@ -7,10 +6,7 @@ test('/assessments/:id sends back a 200 status', () => {
   request(server)
     .get('/assessments/:id')
     .expect(200)
-    .end((err, res) => {
-      expect(err).toBeNull()
-      const $ = cheerio.load(res.text)
-      const pLength = $('body > p').length
-      expect(pLength).toBe(0)
+    .then(res => {
+      expect(res.body.name).toHaveLength()
     })
 })
