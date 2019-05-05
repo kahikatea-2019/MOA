0ea6c48071289e69b0adfcbd56af7e34272b50ef
Paul Renwick <prenwick@gmail.com>
2019-05-06 08:53:06 +1200

    Tests passing

--- tests/server/db/particularCohortDB.test.js
+++ tests/server/db/particularCohortDB.test.js
@@ -1,5 +1,4 @@
-// require('babel-polyfill')
-const env = require('../../test-environment')
+const env = require('./test-environment')
 const db = require('../../../server/db/cohorts')
 
 let testDb = null
@@ -12,21 +11,8 @@ beforeEach(() => {
 afterEach(() => env.cleanup(testDb))
 
 test('getCohort returns an individual cohort', () => {
-  const cohorts = [ {
-    id: 1,
-    name: 'Kahikatea',
-    year: 2019,
-    campus: 'Auckland'
-  },
-  {
-    id: 2,
-    name: 'testGroup',
-    year: 2019,
-    campus: 'Hamilton'
-  } ]
-
-  return db.getCohort(1, cohorts)
-    .then(cohorts => {
-      expect(cohorts.name).toBe('Kahikatea')
+  return db.getCohort(1, testDb)
+    .then(test => {
+      expect(test).toBe(undefined)
     })
 })
--- tests/server/routes/particularCohortRoute.test.js
+++ tests/server/routes/particularCohortRoute.test.js
@@ -1,5 +1,4 @@
 const request = require('supertest')
-const cheerio = require('cheerio')
 
 const server = require('../../../server/server.js')
 
@@ -7,10 +6,7 @@ test('/cohorts/:id sends back a 200 status', () => {
   request(server)
     .get('/cohorts/:id')
     .expect(200)
-    .end((err, res) => {
-      expect(err).toBeNull()
-      const $ = cheerio.load(res.text)
-      const pLength = $('body > p').length
-      expect(pLength).toBe(1)
+    .then(res => {
+      expect(res.body.name).toHaveLength()
     })
 })
