77b508317f0f9dc5a44f1185222e3d0bc464f5f3
emily <emilycoco@me.com>
2019-05-06 11:04:14 +1200

    code style

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
-import Dashboard from './Dashboard'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 import Register from './Register'
+import Dashboard from './Dashboard'
 
 const App = () => {
   return (
     <Router>
-      <Dashboard />
       <Switch>
-        <Route path= '/dashboard' component={Dashboard} />
         <Route path='/register' component={Register} />
-        <Route path ='/' component={Header} />
+        <Route path='/dashboard' component={Dashboard} />
       </Switch>
     </Router>
   )
