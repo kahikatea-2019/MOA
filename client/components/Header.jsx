c5de6355d1bb7a4bf5eb6b1be9b22980d52e9498
Bryce Kehoe <bryce@kehoe.co.nz>
2019-05-07 10:34:32 +1200

    Remove unnecessary constructor (yay class property syntax!)

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 
 export default class Header extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = getInitialData()
-  }
+  state = getInitialData()
 
   render () {
     const { name, surname, cohort } = this.state
