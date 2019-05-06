c2d25fe7ff99b06f2adf958dd4182237ba57d1c6
emily <emilycoco@me.com>
2019-05-06 14:45:16 +1200

    linting - correct import for content

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import Header from './Header'
 
-
-
-export default class Dashboard extends React.Component{
-    render(){
-        return (
-            <React.Fragment>
-            <h1><Header /></h1>
-            <p>YESSSSSS SIS this is working  from the dashboard</p>
-            </React.Fragment>
-        )
-
-        
-    }
+export default class Dashboard extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1><Header /></h1>
+        <p>YESSSSSS SIS this is working  from the dashboard</p>
+      </React.Fragment>
+    )
+  }
 }
