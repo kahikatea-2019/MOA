eea26daa06acfd3be1236b7171d2ae829581095c
Eve Te Uru Henare <evehenare1@gmail.com>
2019-05-07 11:43:43 +1200

    Update Dashboard component

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { recieveAssessmentContent } from '../actions/AssessmentContent'
 
 import Header from './Header'
 import CohortProgress from './CohortProgress'
 import DropDownModule from './DropDownModule'
 import ProgressModule from './ProgressModule'
 
-class Dashboard extends React.Component {
-  render () {
-    
-    return (
-      <React.Fragment>
-        <Header />
-        <CohortProgress />
-        <DropDownModule />
-        <ProgressModule />
-      </React.Fragment>
-    )
-  }
+function Dashboard (props) {
+  const { dispatch } = props
+
+  dispatch(recieveAssessmentContent)
+
+  return (
+    <React.Fragment>
+      <Header />
+      <CohortProgress progress={props.progress}/>
+      <DropDownModule />
+      <ProgressModule />
+    </React.Fragment>
+  )
 }
 
 mapStateToProps = {
