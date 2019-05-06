104a0e420d952e729008d13ed49380c59a2cb36d
Anandani Mala Prakash <anandaniprakash@icloud.com>
2019-05-06 13:18:45 +1200

    removed the conflicts

--- server/server.js
+++ server/server.js
@@ -5,18 +5,9 @@ const cohortsRoute = require('./routes/cohorts')
 
 const server = express()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
-=======
-=======
-
-server.use(express.json())
-server.use(express.static(path.join(__dirname, './public')))
-
->>>>>>> e1ec352b30e181a3345a5513b6ff8f41724c1f10
 const authRoutes = require('./routes/auth')
 
 server.use('/api/v1/auth', authRoutes)
@@ -26,9 +17,4 @@ server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-<<<<<<< HEAD
->>>>>>> 8f9a84037eb5773a290d3b865d54d1faa97e38a6
-=======
-
->>>>>>> e1ec352b30e181a3345a5513b6ff8f41724c1f10
 module.exports = server
