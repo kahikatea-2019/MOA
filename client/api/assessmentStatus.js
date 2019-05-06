e4af393821b210156d810da06195296b01919747
Paul Renwick <prenwick@gmail.com>
2019-05-07 11:25:59 +1200

    Updated api to take id parameter and access status/students/id

--- client/api/assessmentStatus.js
+++ client/api/assessmentStatus.js
@@ -1,7 +1,7 @@
 import request from 'superagent'
 
-export default function getAssessmentStatus () {
-  return request.get('/students/:id')
+export default function getAssessmentStatus (id) {
+  return request.get(`status/students/${id}`)
     .then(res => {
       const statuses = res.body
       return statuses
